ea515f068962c5d9ffe60087e5cdb4df90a93eef
Edgar Mejia <edgarinhm@hotmail.com>
2024-04-28 11:08:51 -0500

    UD03-762 add checking auth status UD03-765 add paths helpers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { signIn, signOut } from "@/actions";
+import { auth } from "@/auth";
 import { Button } from "@nextui-org/react";
+import Profile from "../components/profile";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <main>
-      <Button>Click me!!!</Button>
+      <form action={signIn}>
+        <Button type="submit">{"Sign In"}</Button>
+      </form>
+      <form action={signOut}>
+        <Button type="submit">{"Sign Out"}</Button>
+      </form>
+      {session?.user ? (
+        <pre>{JSON.stringify(session.user)}</pre>
+      ) : (
+        <div>Signed Out</div>
+      )}
+      <Profile />
     </main>
   );
 }
